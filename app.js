ad3ffa4be1227032bd670491918e6dace7b875b5
ing <inyoman_gurnitha@yahoo.com>
2019-03-13 20:46:37 +0700

    21. Separating routes

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-	res.send("hello ...");
-});
+// import routes
+const {getPosts} = require('./routes/post');
+
+app.get("/", getPosts);
 
 const port = 8000;
 app.listen(port, () => {
